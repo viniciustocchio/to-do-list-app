3217550dc1bc91f87e3b17387354c107b2705016
Vinicius Tocchio <80677157+viniciustocchio@users.noreply.github.com>
2022-06-27 15:49:29 +0200

    added jQuery

--- js/script.js
+++ js/script.js
@@ -1,3 +1,35 @@
-src="https://code.jquery.com/jquery-3.3.1.min.js"
-integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
-crossorigin="anonymous"
+function newItem(){
+
+// jQuery Code
+
+//1. Adding a new item to the list of items: 
+  let li = $('<li></li>');
+  let inputValue = $('#input').val();
+  li.append(inputValue);
+
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    $('#list').append(li);
+  }
+
+//2. Crossing out an item from the list of items:
+  function crossOut() {
+    li.toggleClass("strike");
+  }
+
+  li.on("dblclick", function crossOut() {
+    li.toggleClass("strike");
+  });
+
+//3. Adding a delete button
+  let crossOutButton = $('<crossOutButton></crossOutButton>');
+  crossOutButton.append(document.createTextNode('X'));
+  li.append(crossOutButton);
+
+  crossOutButton.on("click", deleteListItem);
+  function deleteListItem(){
+		li.addClass("delete")
+	}
+  $('#list').sortable();
+}
